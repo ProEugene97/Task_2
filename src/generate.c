e87b4736e4ba7cb60ee74fcdb86f5655c9d59bda
ProEugene97 <proj9007@gmail.com>
2019-10-25 23:49:42 +0300

    remove

--- src/generate.c
+++
@@ -1,8 +0,0 @@
-#include "generate.h"
-
-void generate_array(char *const array, int n) {
-    srand(time(NULL));
-    for (int i = 0; i < n; ++i) {
-        array[i] = rand() % (122 - 48 + 1) + 48;
-    }
-}
